dd165105520614da7fbe22e86d46347b1bca4b10
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-09-02 11:55:42 +0600

    add some changes

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
-$('video, audio').mediaelementplayer({
-	// Configuration
-	success: function(media) {
-		var isNative = /html5|native/i.test(media.rendererName);
-
-		var isYoutube = ~media.rendererName.indexOf('youtube');
-
-		// etc.
-	}
-});
-
-
 const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
-const play_button = player.querySelector('.player__button-big');
-const toggle = player.querySelector('.toggle-button');
+const play_button = player.querySelector('.play');
+const toggle = player.querySelector('.toggle');
 
 function togglePlay() {
   const method = video.paused ? 'play' : 'pause';
   video[method]();
 }
+
+video.addEventListener('cliсk', togglePlay);
